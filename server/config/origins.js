dbf46a6ccf0c71978b3cde6bd728c812dc3efd65
Pawan kumar <pawankr16123114@gmail.com>
2026-06-06 17:54:33 +0530

    cors Update

--- server/config/origins.js
+++ server/config/origins.js
@@ -1,6 +1,7 @@
 const DEFAULT_LOCAL_ORIGINS = [
   'http://localhost:3000',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
+  'https://client-beige-ten.vercel.app'
 ];
 
 const normalizeOrigin = (value) => {
